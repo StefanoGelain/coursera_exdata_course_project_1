5eb1b29ad33a2778726984cff56812b33ece789f
StefanoGelain <stefano.gelain@it.pwc.com>
2015-09-13 23:31:56 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,24 @@
+dataset <- "household_power_consumption.txt"
+data <- read.table(dataset,
+                   header=TRUE,
+                   sep=";",
+                   colClasses=c("character", "character", rep("numeric",7)),
+                   na="?")
+
+data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+intervaldates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+data <- subset(data, Date %in% intervaldates)
+
+
+#Plot2
+  
+  png("plot2.png", width=480, height=480)
+  
+  plot(data$Time, data$Global_active_power,
+       type="l",
+       xlab="",
+       ylab="Global Active Power (kilowatts)")
+  
+  dev.off()
